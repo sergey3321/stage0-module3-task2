c93e3a169442f2d0cd045aa0181a89f5d8e30253
Sergey234 <mostov.sergey88@gmail.com>
2022-10-18 20:28:38 +0300

    stage 0 Module 3 - Задача №2. Ключевые моменты

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,15 @@
 package lang.print.gaps.task2;
 
 public class advancedNamingConvention {
-    private static final int adult_age = 18;
-
-    private int AGE;
+    private static final int ADULT_AGE = 18;
+    private int age;
     private int phoneNumber;
 
-    void CAllToFriend() {
+    void сallToFriend() {
         //some code
     }
 
-    void callByNumber(int Number) {
+    void сallByNumber(int Number) {
         //some code
     }
-}
+    }
